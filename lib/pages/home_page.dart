2dc8c85707105c95bbb996f3920c09849deb35d2
Johanna Perso <johanna.stoicanescu@gmail.com>
2025-09-04 15:00:24 +0200

    app init + navigation and pages + init firebase + add authentification

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../widgets/drawer.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('E Commerce App')),
+      drawer: const AppDrawer(),
+      body: const Center(child: Text('TODO: Contenu de la page d\'accueil')),
+    );
+  }
+}
