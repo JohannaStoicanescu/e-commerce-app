6c9883e87ed8694a2802fb0d623b5a962e9c3a46
Johanna Perso <johanna.stoicanescu@gmail.com>
2025-09-24 09:51:23 +0200

    todos

---
+++ lib/ui/pages/orders/orders_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../_global_widgets/drawer.dart';
+
+class OrdersPage extends StatefulWidget {
+  const OrdersPage({super.key});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Historique des commandes')),
+      drawer: const AppDrawer(),
+      body: const Center(
+          child: Text('TODO: Contenu de la page d\'historique des commandes')),
+    );
+  }
+}
